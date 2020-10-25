ddac5a5101da4fe7d899e35025c0003130aa98ac
Hasmik Varosyan <hasvart@gmail.com>
2020-10-24 19:09:57 -0500

    added a new project to the grid

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function myFunction() {
     document.getElementById("email").innerHTML = "E-mail address: hasvart@gmail.com";
   }
+
+function openSlideMenu(){
+document.getElementById('menu').style.width = '250px';
+
+}
+ 
+ function closeSlidemenu(){
+   document.getElementById('menu').style.width ='0';
+ }
+  
