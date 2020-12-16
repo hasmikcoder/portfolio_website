62896f332bb89a20e1512218c41805d61d6829b0
Hasmik Varosyan <hasvart@gmail.com>
2020-12-16 13:27:32 -0600

    added carousel_slideshow

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+//e-mail_social
 function myEmail() {
     document.getElementById("email").innerHTML = "E-mail address: hasvart@gmail.com";
   }
 
 
-
+//humburger
   function myFunction() {
     var x = document.getElementById("side-nav");
     if (x.style.display === "block") {
@@ -12,3 +13,34 @@ function myEmail() {
       x.style.display = "block";
     }
   }
+
+  
+ //slideshow_carousel
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
