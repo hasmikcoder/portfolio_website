d455e5280c18069cb922497063eb2e0f1b5ba2f6
Hasmik Varosyan <hasvart@gmail.com>
2021-01-05 21:33:13 -0600

    hamburger menu is working

--- script.js
+++ script.js
@@ -4,43 +4,42 @@ function myEmail() {
   }
 
 
-//humburger
-  function myFunction() {
+//hamburger
+  /*function myFunction() {
     var x = document.getElementById("side-nav");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
-  }
+  }*/
+
+  
+//hamburger menu
+  const btn = document.getElementById("btn");
+  const nav = document.getElementById("nav");
+  
+  btn.addEventListener("click", () => {
+      nav.classList.toggle("active");
+      btn.classList.toggle("active");
+  });
+
+  
+
 
   
  //slideshow_carousel
-  var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+ var slideIndex = 0;
+ showSlides();
+ 
+ function showSlides() {
+   var i;
+   var slides = document.getElementsByClassName("mySlides");
+   for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+   }
+   slideIndex++;
+   if (slideIndex > slides.length) {slideIndex = 1}
+   slides[slideIndex-1].style.display = "block";
+   setTimeout(showSlides, 2000); // Change image every 2 seconds
+ }
